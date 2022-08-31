befc86ef94a5f2373760df6e9a658f3a06436f2f
Gabriel Carlos Carvalho <gabrielcarlos@MacBook-Pro-de-Gabriel.local>
2022-08-31 17:01:45 -0400

    commit

---
+++ ReactGram/frontend/src/components/Message.js
@@ -0,0 +1,11 @@
+import "./Message.css"
+
+const Message = ({msg, type}) => {
+  return (
+    <div classname={`message ${type}`}>
+        <p>{msg}</p>
+    </div>
+  )
+}
+
+export default Message
